4457069d89500d5b54fe8f6b8cb597b9d56aa482
Layla Alharbi <laylaAlharbi@Laylas-MacBook.local>
2021-07-14 03:58:25 +0300

    initial commit

---
+++ Migrations/20210714005336_HobbyPublic.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FavoriteMovies.Migrations
+{
+    public partial class HobbyPublic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
